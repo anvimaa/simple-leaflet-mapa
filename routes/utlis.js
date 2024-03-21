53c1c3aabd888043b57cb5164abb69504b5b00cb
António Mantente <70594573+anvimaa@users.noreply.github.com>
2024-03-21 15:42:29 +0100

    Update utlis.js

--- routes/utlis.js
+++ routes/utlis.js
@@ -6,13 +6,6 @@ let formatDate = {
   minute: "numeric",
 };
 
-function makeButonEditDelete(id, model) {
-  return `<button class="btn btn-info"onclick="onEdit('${id}','${model}')">
-        <i class="fa fa-pen"></i></button>
-        <button onclick="onDelete('${id}','${model}')" class="btn btn-danger">
-        <i class="fa fa-trash"></i></button>`;
-}
-
 module.exports = {
   makeButonEditDelete,
   formatDate,
